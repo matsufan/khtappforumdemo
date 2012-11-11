5c0cad419673d616033c7ce6510dea02fd086ae4
Keith <keith@rathercoding.com>
2012-11-11 15:41:59 +0800

    remove public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumDemo::Application.config.secret_token = 'b3b2c5f653577502ccaf3fe5261a60b9bb8e836fb3c1c3f40efffba34e959779b7bfc2df630ff4704c90703828c47c58c673829587c5860efa84e6516b36cb06'
